940f5ff839882f0dd5305e5330e74362c0b3470e
rightbrainpapi <dahuemaen@gmail.com>
2020-02-03 01:15:02 -0500

    adding a slide show and a timer to reset the state of the slide

--- main.js
+++ main.js
@@ -87,3 +87,53 @@ function addBannerListener() {
     });
     
 }
+
+
+
+
+
+
+
+////////////////////////////////////////////////////
+//////////////////// Slide Show ////////////////////
+////////////////////////////////////////////////////
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    // .img-anchor:hover:after
+    var slidesCon = document.getElementById("slideCon");
+    slidesCon.classList.remove("img-anchor");
+        // slides[i].classList.remove("img-anchor");
+      slides[i].style.display = "none";  
+      setTimeout(mySlideTimer, 5000);
+      
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+function mySlideTimer() {
+    var slidesCon = document.getElementById("slideCon");
+    slidesCon.classList.add("img-anchor");
+    console.log("We are working")
+  }
